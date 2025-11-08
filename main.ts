7892d3342fd7278f7cfa3fc9741722fcf735b4e5
shdi14 <116509953+shdi14@users.noreply.github.com>
2025-11-08 10:43:36 +0300

    Обновить main.blocks, main.ts, images.g.jres

--- main.ts
+++ main.ts
@@ -51,7 +51,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`переход`, function (sp
         room = 3
         tiles.setCurrentTilemap(tilemap`уровень0`)
         mySprite2 = sprites.create(assets.image`dawdaw`, SpriteKind.cubird_enemy_1)
-        mySprite2.setPosition(64, 59)
     }
     if (room == 2) {
         tiles.setCurrentTilemap(tilemap`1 комната`)
